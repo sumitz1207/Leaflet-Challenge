1f4677d19badc4a42daa4e1f3590a81e34b7ab49
Sumit Patel <sumitz1207@gmail.com>
2021-03-03 23:29:55 -0500

    added tile layer to logic javascript

---
+++ static/js/logic.js
@@ -0,0 +1,14 @@
+
+//star with tile layer using mapbox dark map from openstreetmap
+var tile = L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 15,
+      zoomOffset: -1,
+      id: "mapbox/dark-v10",
+      accessToken: API_KEY
+    }
+  );
