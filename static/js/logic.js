7bf2ae99bc039e3ed9dc1f55337e27d7a6922052
Sumit Patel <sumitz1207@gmail.com>
2021-03-03 23:30:48 -0500

    link to chosen all month earth json and center, zoom of map

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,3 +12,12 @@ var tile = L.tileLayer(
       accessToken: API_KEY
     }
   );
+
+//link to the earthquake all month geojson file
+var earthquakelink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+//create map variable with center and zoom variables
+var map = L.map("map", {
+  center: [33.14, -96],
+  zoom: 3
+});
